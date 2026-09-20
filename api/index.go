14cd953b21b4e2ce9e635e98e4089cf8e3f41df1
rishabhdas1006 <prin100603@gmail.com>
2026-09-20 20:01:14 +0530

    bug fix

--- api/index.go
+++ api/index.go
@@ -3,11 +3,19 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"styleai-backend/pkg/server"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	setCORSHeaders(w, r)
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
 	if r.URL.Path == "/health" {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
@@ -22,4 +30,19 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.ServeHTTP(w, r)
+}
+
+func setCORSHeaders(w http.ResponseWriter, r *http.Request) {
+	frontendURL := os.Getenv("FRONTEND_URL")
+	if frontendURL == "" {
+		return
+	}
+
+	if r.Header.Get("Origin") == frontendURL {
+		w.Header().Set("Access-Control-Allow-Origin", frontendURL)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		w.Header().Set("Vary", "Origin")
+	}
 }
